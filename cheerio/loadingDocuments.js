96902a7c3d7db6e612fcd9ad1c49fd37edb20f47
Darwin Diaz <152736334+darwin-dev-diaz@users.noreply.github.com>
2024-12-19 14:44:20 -0500

    learn some shi

--- cheerio/loadingDocuments.js
+++ cheerio/loadingDocuments.js
@@ -1,5 +1,23 @@
 const cheerio = require("cheerio");
 
+// load is the most basic way to load something. Takes an html string and gives us a cheerio obj
+// by default it includes the <html> and <head> tag. use third arg to disable this
+// let $ = cheerio.load("<h1>Hello, world!</h1>", null, false);
+// console.log($.html());
 
-const $ = cheerio.load("<h1>Hello, world!</h1>");
-console.log($("h1").text());
+
+// this is how you load from buffer
+const fs = require("fs");
+const buffer = fs.readFileSync("document.html");
+$ = cheerio.loadBuffer(buffer);
+
+console.log($("title").text());
+
+
+// this is how you load from URL
+const run = async () => {
+  $ = await cheerio.fromURL("https://example.com");
+  console.log($.html());
+};
+
+run();
